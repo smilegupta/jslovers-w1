0d47d48c8b1db5c58d2cc200071f882c288a4ef1
Smile Gupta <smile.gupta@razorpay.com>
2024-03-30 15:07:11 +0530

    Feat: updated the long polling

--- live-coding/longPolling/server.js
+++ live-coding/longPolling/server.js
@@ -1,13 +1,10 @@
 const express = require("express");
-const cors = require("cors");
 
 const app = express();
 
 app.use(express.json());
-app.use(cors());
 
 let posts = [];
-const waitingClients = [];
 
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/index.html");
@@ -19,26 +16,6 @@ app.get("/style.css", (req, res) => {
 
 app.get("/script.js", (req, res) => {
   res.sendFile(__dirname + "/script.js");
-});
-
-app.get("/api/posts", (req, res) => {
-  if (req.query.longPolling === "true") {
-    waitingClients.push(res);
-  } else {
-    res.json({ posts });
-  }
-});
-
-app.post("/api/post", (req, res) => {
-  const { user, text } = req.body;
-  const newPost = { user, text, timestamp: new Date() };
-  posts.push(newPost);
-
-  waitingClients.forEach((client) => {
-    client.json({ posts });
-  });
-
-  res.json({ success: true });
 });
 
 const port = 3000;
